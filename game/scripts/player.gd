29daed60600d946b4b17ebbb48eb389967e481ba
Otchi <adamgragnic@gmail.com>
2026-02-16 10:54:21 -0500

    Add missing files

---
+++ game/scripts/player.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+@onready var gd_player: GDPlayer = $GDPlayer
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var gd_network_manager: GDNetworkManager
+
+func _jump() -> void :
+	velocity.y = JUMP_VELOCITY
+	
+func _ready() -> void:
+	self.gd_network_manager = get_parent()
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		_jump()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	move_and_slide()
+	
+	gd_network_manager.replicate_new_movement(gd_player, get_transform().get_origin())
